23f9595e1abc425a350973556c3100bb158c0d11
Sergio Sanchez <ssantiago065@gmail.com>
2025-01-21 20:36:19 -0600

    Add lines to board and define matrix of squares

--- TicTacToe.py
+++ TicTacToe.py
@@ -13,16 +13,37 @@ pygame.display.set_caption("UNBEATABLE TIC TAC TOE")
 screen.fill(BACKGROUND)
 
 class Board:
-    pass
+    
+    def __init__(self):
+        self.squares = np.zeros( (ROWS,COLUMNS) )
+
+    def mark_square(self, row, column, player):
+        self.squares[row][column] = player 
+
 
 class MiniMax:
     pass
 
 class Game:
-    pass
+    
+    def __init__(self):
+        self.board = Board()
+        self.player = 1
+        self.show_lines()
+
+    def show_lines(self):
+        #Vertical
+        pygame.draw.line(screen, LINE, (SQUARESIZE,0), (SQUARESIZE,HEIGHT), LINEWIDTH)
+        pygame.draw.line(screen, LINE, (WIDTH-SQUARESIZE,0), (WIDTH-SQUARESIZE,HEIGHT), LINEWIDTH)
+
+        #Horizontal
+        pygame.draw.line(screen, LINE, (0,SQUARESIZE), (WIDTH,SQUARESIZE), LINEWIDTH)
+        pygame.draw.line(screen, LINE, (0,HEIGHT-SQUARESIZE), (WIDTH,HEIGHT-SQUARESIZE), LINEWIDTH)
+    
 
 def main():
-    pass
+    
+    game = Game()
 
     while True:
         for event in pygame.event.get():
