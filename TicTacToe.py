4ce35a57ebfd2a9f9b614bf7789cf77441fb737f
Sergio Sanchez <ssantiago065@gmail.com>
2025-01-21 20:11:50 -0600

    Create class structure

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,9 +1,36 @@
 import sys
 import pygame
+import random
+import copy
+import numpy as np
 
 from constants import * 
+
 #Pygame setup
 pygame.init()
-screen = pygame.display((WIDTH,HEIGHT))
+screen = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption("UNBEATABLE TIC TAC TOE")
+screen.fill(BACKGROUND)
 
+class Board:
+    pass
+
+class MiniMax:
+    pass
+
+class Game:
+    pass
+
+def main():
+    pass
+
+    while True:
+        for event in pygame.event.get():
+
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        pygame.display.update()
+
+main()
--- constants.py
+++ constants.py
@@ -1,2 +1,5 @@
 WIDTH = 600
 HEIGHT = 600
+
+#Colors
+BACKGROUND = (28,170,156)
