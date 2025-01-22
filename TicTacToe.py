a9a24bf950929f11ec6383fe11b1041f1bb6e2c7
Sergio Sanchez <ssantiago065@gmail.com>
2025-01-21 22:32:37 -0600

    Add random function to implement game against random moves

--- TicTacToe.py
+++ TicTacToe.py
@@ -16,22 +16,87 @@ class Board:
     
     def __init__(self):
         self.squares = np.zeros( (ROWS,COLUMNS) )
+        self.empty_squares = self.squares
+        self.marked_squares = 0
+
+    def final_state(self):
+        
+        #Vertical wins
+        for column in range(COLUMNS):
+            if self.squares[0][column] == self.squares[1][column] == self.squares[2][column] != 0:
+                return self.squares[0][column]
+
+        #Horizontal wins
+        for row in range(ROWS):
+            if self.squares[row][0] == self.squares[row][1] == self.squares[row][2] != 0:
+                return self.squares[row][0]
+
+        #Descending diagonal
+        if self.squares[0][0] == self.squares[1][1] == self.squares[2][2] != 0:
+            return self.squares[1][1]
+        
+        #Ascending diagonal
+        if self.squares[2][0] == self.squares[1][1] == self.squares[0][2] != 0:
+            return self.squares[1][1]
+        
+        return 0
 
     def mark_square(self, row, column, player):
         self.squares[row][column] = player 
+        self.marked_squares += 1
 
-    def is_empty(self, row, column):
+    def square_empty(self, row, column):
         return self.squares[row][column] == 0
+    
+    def get_empty_squares(self):
+        empty_squares = []
+        for row in range(ROWS):
+            for column in range(COLUMNS):
+                if self.square_empty(row, column):
+                    empty_squares.append( (row,column) )
 
+        return empty_squares
+    
+    def is_full(self):
+        return self.marked_squares == 9
+    
+    def is_empty(self):
+        return self.marked_squares == 0
+    
+
+    
 
 class MiniMax:
-    pass
+    
+    def __init__(self,mode=0,player=2):
+        self.mode = mode
+        self.player = player
 
+    def rand(self,board):
+        empty_squares = board.get_empty_squares()
+        index = random.randrange(0, len(empty_squares))
+
+        return empty_squares[index]
+    
+    def eval(self,main_board):
+        if self.mode == 0:
+            #random
+            move = self.rand(main_board)
+
+        else:
+            #minimax
+            pass
+
+        return move
+    
 class Game:
     
     def __init__(self):
         self.board = Board()
-        self.player = 1 
+        self.minimax = MiniMax()
+        self.player = 1 # 1 - cross 2 - circle
+        self.gamemode = "minimax" #pvp or minimax
+        self.running = True
         self.show_lines()
 
     def show_lines(self):
@@ -69,6 +134,7 @@ def main():
     
     game = Game()
     board = game.board
+    minimax = game.minimax
 
     #Game loop
     while True:
@@ -83,10 +149,20 @@ def main():
                 row = row // SQUARESIZE
                 column = column // SQUARESIZE
                 
-                if board.is_empty(row,column):
+                if board.square_empty(row,column):
                     board.mark_square(row,column,game.player)
                     game.draw_fig(row, column)
                     game.change_turn()
+            
+        if game.gamemode == "minimax" and game.player == minimax.player:
+            pygame.display.update()
+
+            row, column = minimax.eval(board)
+            board.mark_square(row,column,minimax.player)
+            game.draw_fig(row, column)
+            game.change_turn()
+            
+
                     
 
                 
